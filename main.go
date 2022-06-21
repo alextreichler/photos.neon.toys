84fb413643c6e058329e648957d002e5d3b85a46
Alex Treichler <alex.trei@gmail.com>
2022-06-21 14:36:34 -0700

    added the embed template

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/straightbuggin/photos.neon.toys/controllers"
+	"github.com/straightbuggin/photos.neon.toys/templates"
 	"github.com/straightbuggin/photos.neon.toys/views"
 )
 
@@ -14,19 +14,12 @@ func main() {
 
 	r := chi.NewRouter()
 
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
--- views/template.go
+++ views/template.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -21,10 +22,25 @@ func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	}
 }
 
+func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
+	htmlTpl, err := template.ParseFS(fs, pattern...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{htmlTpl: htmlTpl}, nil
+}
+
 func Parse(filepath string) (Template, error) {
 	htmlTpl, err := template.ParseFiles((filepath))
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
 	return Template{htmlTpl: htmlTpl}, nil
+}
+
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
