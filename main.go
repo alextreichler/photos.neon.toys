9d67dd981f0a5c521e9f58f85212055496c3e5f1
straightbuggin <alex.trei@gmail.com>
2023-05-26 13:52:31 -0700

    worked on creating session tokens

--- main.go
+++ main.go
@@ -32,9 +32,12 @@ func main() {
 	userService := models.UserService{
 		DB: db,
 	}
-
+	sessionService := models.SessionService{
+		DB: db,
+	}
 	usersC := controllers.Users{
-		UserService: &userService,
+		UserService:    &userService,
+		SessionService: &sessionService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(
 		templates.FS,
