879faf68bc5be0148923a2bd4ee9def3dbcdabf6
straightbuggin <alex.trei@gmail.com>
2023-05-30 15:36:03 -0700

    added foreign key for sessions table

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -1,7 +1,8 @@
 CREATE TABLE sessions (
   id SERIAL PRIMARY KEY,
   user_id INT UNIQUE,
-  token_hash TEXT UNIQUE NOT NULL    
+  token_hash TEXT UNIQUE NOT NULL
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 
